397db22d980ea970c8b4eb72824db8a39cdbbae7
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2024-06-23 23:42:36 +0200

    Start fleshing out cmds between Pi-0 and Pico

--- source/shared/SerialCommand.h
+++ source/shared/SerialCommand.h
@@ -29,22 +29,50 @@ enum SerialCommand : uint8_t
 {
     kNullCmd                = 0x00,
 
-    // Timer events
-    kTimerNavUpdate         = 0x10,             // Navigation timer (1/8 sec)
-    kTimer1_4s              = 0x20,             // 1/4 sec timer event
-    kTimer1s                = 0x21,             // 1 sec timer event
-    kTimer8s                = 0x22,             // 8 sec timer event
+    // Commands (to Pico); acknowledged from Pico with same Cmd & second byte (non-zero 2nd byte -> error code)
+    kPicoReady              = 0x01,             // Sent by Pico once ready (non-zero 2nd byte -> error code)
+    kPauseCmd               = 0x02,             // Pico to pause event processing
+    kResumeCmd              = 0x03,             // Pico to resume event processing  
+    kResetCmd               = 0x09,             // Pico to reses itself (ack by sending kPicoReady)
+
+    // Timer events (from Pico)
+    kTimerNavUpdate         = 0x10,             // Navigation timer (1/8 sec) (info in following bytes)
+    kTimer1_4s              = 0x11,             // 1/4 sec timer event (2nd byte -> count)
+    kTimer1s                = 0x12,             // 1 sec timer event (2nd byte -> count)
+    kTimer8s                = 0x13,             // 8 sec timer event (2nd byte -> count)
+
+    // Navigation info
+    kDrivingStatusUpdate    = 0x20,             // From Pi-0 to Pico (2nd byte provides driving status)
+    kLeftEncoderUpdate      = 0x21,             // Pico sends update containing left encoder count
+    kRightEncoderUpdate     = 0x22,             // Pico sends update containing right encoder count
+
+    // Battery info
+    kRequestBatteryLevel    = 0x30,             // Pico to reply same with IC battery V in following bytes
+    kBatteryLowAlert        = 0x31,             // Pico sends alert about IC battery low
+    kRequestMotorBattLevel  = 0x32,             // Pico to reply same with motor battery V in following bytes
+    kMotorBattLowAlert      = 0x33,             // Pico sends alert about motor battery low
+
+    // Error reports
+    kErrorReportFromPico    = 0xE0,             // Pico sends error code (int) in following bytes
 
 
     // Debugging events
-    kIdentifyCodeRequest    = 0xF0,
-    kIdentifyCore           = 0xF1,
+    kIdentifyCodeRequest    = 0xF0,             // Pi-0 sends to Pico asking to identify core running uart
+    kIdentifyCore           = 0xF1,             // Pico reports which core is running uart (2nd byte)
 
     kExtendCmd              = 0xFF
 };
 
 
+enum SerialExtendedCmd : uint8_t
+{
+    kNullExtCmd             = 0x00,
 
+    // Debugging extended cmds
+    kTestExtendedCmd        = 0xF0,
+
+    kTriggerError           = 0xFF
+};
 
 
 
