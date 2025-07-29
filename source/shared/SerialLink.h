f8f4d283eb03bea9cfcd43e1cf336f4eae850444
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2025-07-28 21:13:39 -0400

    Start conversion to base SerialLink class (abstract)

--- source/shared/SerialLink.h
+++ source/shared/SerialLink.h
@@ -1,9 +1,9 @@
 /*
     SerialLink.h - Tools for CARRT3 communications over serial (UART) link
     between the RPI and Pico.  This file is shared by both the
-    RPI and Pico code bases.
+    RPI and Pico code bases.  
 
-    Copyright (c) 2024 Igor Mikolic-Torreira.  All right reserved.
+    Copyright (c) 2025 Igor Mikolic-Torreira.  All right reserved.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,48 +26,53 @@
 
 
 
-#include <stdint.h>
+#include <cstdint>
 
 
 
-namespace SerialLink
+class SerialLink
 {
+
+public:
+
+    virtual ~SerialLink();
+
+    virtual bool isReadable() = 0;
+
+    virtual uint8_t getByte() = 0;
+    virtual void get4Bytes( uint8_t* c ) = 0;
+
+    virtual void putByte( uint8_t c ) = 0;
+    virtual void put4Bytes( uint8_t* c ) = 0;
+
+    inline uint8_t getMsgType()             { return getByte(); }
+    inline int getInt()                     { Transfer t{}; get4Bytes( t.c ); return t.i; }
+    inline uint32_t getUInt32()             { Transfer t{}; get4Bytes( t.c ); return t.u; }
+    inline float getFloat()                 { Transfer t{}; get4Bytes( t.c ); return t.f; }
+
+    inline void putMsgType( uint8_t cmd )   { putByte( cmd ); }
+    inline void put( char c )               { putByte( c ); }
+    inline void put( uint8_t c )            { putByte( c ); }
+    inline void put( int i )                { Transfer t{}; t.i = i; put4Bytes( t.c ); }
+    inline void put( uint32_t u )           { Transfer t{}; t.u = u; put4Bytes( t.c ); }
+    inline void put( float f )              { Transfer t{}; t.f = f; put4Bytes( t.c ); }
+
+
+protected:
+
+    SerialLink()                            {}
+
+
+private:
 
     union Transfer
     {
-        uint8_t     c[4];
-        int         i;
-        uint32_t    u;
-        float       f;
+        std::uint8_t    c[4];
+        int             i;
+        std::uint32_t   u;
+        float           f;
     };
-
-    void openSerialLink();
-    void closeSerialLink();
-
-    bool isReadable();
-
-    uint8_t getByte();
-    void get4Bytes( uint8_t* c );
-
-    inline uint8_t getMsg()  { return getByte(); }
-
-    inline int getInt()  { SerialLink::Transfer t; SerialLink::get4Bytes( t.c ); return t.i; }
-
-    inline uint32_t getUInt32()  { SerialLink::Transfer t; SerialLink::get4Bytes( t.c ); return t.u; }
-
-    inline float getFloat()  { SerialLink::Transfer t; SerialLink::get4Bytes( t.c ); return t.f; }
-
-
-    void putByte( uint8_t c );
-    void put4Bytes( uint8_t* c );
-
-    inline void putMsg( uint8_t cmd )  { putByte( cmd ); }
-    inline void put( char c )   { SerialLink::putByte( c ); }
-    inline void put( uint8_t c )   { SerialLink::putByte( c ); }
-    inline void put( int i )  { SerialLink::Transfer t; t.i = i; SerialLink::put4Bytes( t.c ); }
-    inline void put( uint32_t u )  { SerialLink::Transfer t; t.u = u; SerialLink::put4Bytes( t.c ); }
-    inline void put( float f )  { SerialLink::Transfer t; t.f = f; SerialLink::put4Bytes( t.c ); }
-
+    
 };
 
 
