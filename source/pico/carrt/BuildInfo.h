32e1cd84ecb4c2ee9abbafbf2c1e54a32417dc9d
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2026-03-01 22:35:56 -0500

    Clean up and format file

--- source/pico/carrt/BuildInfo.h
+++ source/pico/carrt/BuildInfo.h
@@ -17,37 +17,28 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-
-
-
 #ifndef BuildInfo_h
 #define BuildInfo_h
 
+#include <cstdint>
 
-#include "GitHeadHash.h"
 #include "CarrtPicoDefines.h"
-
+#include "GitHeadHash.h"
 
 /*
  * Define the SHA1 hash
  *
  */
 
- #define CARRTPICO_GIT_HEAD_SHA1_FULL      GIT_CURRENT_HEAD_SHA1_FULL
- #define CARRTPICO_GIT_HEAD_SHA1_SHORT     GIT_CURRENT_HEAD_SHA1_SHORT
-
+#define CARRTPICO_GIT_HEAD_SHA1_FULL GIT_CURRENT_HEAD_SHA1_FULL
+#define CARRTPICO_GIT_HEAD_SHA1_SHORT GIT_CURRENT_HEAD_SHA1_SHORT
 
 /*
  * Convert the CARRT version number into a string using only the preprocessor
  *
  */
 
-
-#define CARRTPICO_VERSION           "v" CARRTPICO_VERSION_STR
-
-
-
+#define CARRTPICO_VERSION "v" CARRTPICO_VERSION_STR
 
 /*
  * Determine the features compiled into this CARRT build and encapsulate that as
@@ -56,93 +47,82 @@
  */
 
 #if DEBUGCARRTPICO
-#define CARRT_DEBUG_STR  "D"
+    #define CARRT_DEBUG_STR "D"
 #else
-#define CARRT_DEBUG_STR  ""
+    #define CARRT_DEBUG_STR ""
 #endif
 
 #if USE_CARRTPICO_STDIO
-#define CARRT_STDIO_STR  "S"
+    #define CARRT_STDIO_STR "S"
 #else
-#define CARRT_STDIO_STR  ""
+    #define CARRT_STDIO_STR ""
 #endif
 
-
-#define CARRTPICO_FEATURES   CARRT_STDIO_STR " " CARRT_DEBUG_STR 
-
-
-
-
+#define CARRTPICO_FEATURES CARRT_STDIO_STR " " CARRT_DEBUG_STR
 
 /*
  * Following code transforms the __DATE__ macro from Mmm [D]D YYYY format to my
  * preferred format of YYYYMMDD.  So that Dec  2 2018 becomes 20181202.
  *
- * This is a complicated as it is because the transformation is done
+ * This is complicated as it is because the transformation is done
  * entirely in the preprocessor, with no run-time or memory overhead.
  *
  */
 
+#define CARRT_BUILD_YEAR __DATE__[ 7 ], __DATE__[ 8 ], __DATE__[ 9 ], __DATE__[ 10 ]
 
-#define CARRT_BUILD_YEAR    __DATE__[7], __DATE__[8], __DATE__[9], __DATE__[10]
+#define DAY_FIRST_DIGIT ( __DATE__[ 4 ] == ' ' ) ? '0' : __DATE__[ 4 ]
 
-#define DAY_FIRST_DIGIT     (__DATE__[4] == ' ') ? '0' : __DATE__[4]
+#define CARRT_BUILD_DAY DAY_FIRST_DIGIT, __DATE__[ 5 ]
 
-#define CARRT_BUILD_DAY     DAY_FIRST_DIGIT, __DATE__[5]
-
-
-#define BUILD_MONTH_IS_JAN (__DATE__[0] == 'J' && __DATE__[1] == 'a' && __DATE__[2] == 'n')
-#define BUILD_MONTH_IS_FEB (__DATE__[0] == 'F')
-#define BUILD_MONTH_IS_MAR (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'r')
-#define BUILD_MONTH_IS_APR (__DATE__[0] == 'A' && __DATE__[1] == 'p')
-#define BUILD_MONTH_IS_MAY (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'y')
-#define BUILD_MONTH_IS_JUN (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'n')
-#define BUILD_MONTH_IS_JUL (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'l')
-#define BUILD_MONTH_IS_AUG (__DATE__[0] == 'A' && __DATE__[1] == 'u')
-#define BUILD_MONTH_IS_SEP (__DATE__[0] == 'S')
-#define BUILD_MONTH_IS_OCT (__DATE__[0] == 'O')
-#define BUILD_MONTH_IS_NOV (__DATE__[0] == 'N')
-#define BUILD_MONTH_IS_DEC (__DATE__[0] == 'D')
-
+#define BUILD_MONTH_IS_JAN ( __DATE__[ 0 ] == 'J' && __DATE__[ 1 ] == 'a' && __DATE__[ 2 ] == 'n' )
+#define BUILD_MONTH_IS_FEB ( __DATE__[ 0 ] == 'F' )
+#define BUILD_MONTH_IS_MAR ( __DATE__[ 0 ] == 'M' && __DATE__[ 1 ] == 'a' && __DATE__[ 2 ] == 'r' )
+#define BUILD_MONTH_IS_APR ( __DATE__[ 0 ] == 'A' && __DATE__[ 1 ] == 'p' )
+#define BUILD_MONTH_IS_MAY ( __DATE__[ 0 ] == 'M' && __DATE__[ 1 ] == 'a' && __DATE__[ 2 ] == 'y' )
+#define BUILD_MONTH_IS_JUN ( __DATE__[ 0 ] == 'J' && __DATE__[ 1 ] == 'u' && __DATE__[ 2 ] == 'n' )
+#define BUILD_MONTH_IS_JUL ( __DATE__[ 0 ] == 'J' && __DATE__[ 1 ] == 'u' && __DATE__[ 2 ] == 'l' )
+#define BUILD_MONTH_IS_AUG ( __DATE__[ 0 ] == 'A' && __DATE__[ 1 ] == 'u' )
+#define BUILD_MONTH_IS_SEP ( __DATE__[ 0 ] == 'S' )
+#define BUILD_MONTH_IS_OCT ( __DATE__[ 0 ] == 'O' )
+#define BUILD_MONTH_IS_NOV ( __DATE__[ 0 ] == 'N' )
+#define BUILD_MONTH_IS_DEC ( __DATE__[ 0 ] == 'D' )
 
 #define CARRT_BUILD_MONTH_FIRST_DIGIT \
-    ( \
-        (BUILD_MONTH_IS_JAN) ? '0' : \
-        (BUILD_MONTH_IS_FEB) ? '0' : \
-        (BUILD_MONTH_IS_MAR) ? '0' : \
-        (BUILD_MONTH_IS_APR) ? '0' : \
-        (BUILD_MONTH_IS_MAY) ? '0' : \
-        (BUILD_MONTH_IS_JUN) ? '0' : \
-        (BUILD_MONTH_IS_JUL) ? '0' : \
-        (BUILD_MONTH_IS_AUG) ? '0' : \
-        (BUILD_MONTH_IS_SEP) ? '0' : \
-        (BUILD_MONTH_IS_OCT) ? '1' : \
-        (BUILD_MONTH_IS_NOV) ? '1' : \
-        (BUILD_MONTH_IS_DEC) ? '1' : \
-        /* error default */    '9' \
-    )
-
+    ( ( BUILD_MONTH_IS_JAN )   ? '0'  \
+      : ( BUILD_MONTH_IS_FEB ) ? '0'  \
+      : ( BUILD_MONTH_IS_MAR ) ? '0'  \
+      : ( BUILD_MONTH_IS_APR ) ? '0'  \
+      : ( BUILD_MONTH_IS_MAY ) ? '0'  \
+      : ( BUILD_MONTH_IS_JUN ) ? '0'  \
+      : ( BUILD_MONTH_IS_JUL ) ? '0'  \
+      : ( BUILD_MONTH_IS_AUG ) ? '0'  \
+      : ( BUILD_MONTH_IS_SEP ) ? '0'  \
+      : ( BUILD_MONTH_IS_OCT ) ? '1'  \
+      : ( BUILD_MONTH_IS_NOV ) ? '1'  \
+      : ( BUILD_MONTH_IS_DEC ) ? '1'  \
+                               : /* error default */ '9' )
 
 #define CARRT_BUILD_MONTH_SECOND_DIGIT \
-    ( \
-        (BUILD_MONTH_IS_JAN) ? '1' : \
-        (BUILD_MONTH_IS_FEB) ? '2' : \
-        (BUILD_MONTH_IS_MAR) ? '3' : \
-        (BUILD_MONTH_IS_APR) ? '4' : \
-        (BUILD_MONTH_IS_MAY) ? '5' : \
-        (BUILD_MONTH_IS_JUN) ? '6' : \
-        (BUILD_MONTH_IS_JUL) ? '7' : \
-        (BUILD_MONTH_IS_AUG) ? '8' : \
-        (BUILD_MONTH_IS_SEP) ? '9' : \
-        (BUILD_MONTH_IS_OCT) ? '0' : \
-        (BUILD_MONTH_IS_NOV) ? '1' : \
-        (BUILD_MONTH_IS_DEC) ? '2' : \
-        /* error default */    '9' \
-    )
+    ( ( BUILD_MONTH_IS_JAN )   ? '1'   \
+      : ( BUILD_MONTH_IS_FEB ) ? '2'   \
+      : ( BUILD_MONTH_IS_MAR ) ? '3'   \
+      : ( BUILD_MONTH_IS_APR ) ? '4'   \
+      : ( BUILD_MONTH_IS_MAY ) ? '5'   \
+      : ( BUILD_MONTH_IS_JUN ) ? '6'   \
+      : ( BUILD_MONTH_IS_JUL ) ? '7'   \
+      : ( BUILD_MONTH_IS_AUG ) ? '8'   \
+      : ( BUILD_MONTH_IS_SEP ) ? '9'   \
+      : ( BUILD_MONTH_IS_OCT ) ? '0'   \
+      : ( BUILD_MONTH_IS_NOV ) ? '1'   \
+      : ( BUILD_MONTH_IS_DEC ) ? '2'   \
+                               : /* error default */ '9' )
 
-#define CARRTPICO_BUILD_DATE     CARRT_BUILD_YEAR, CARRT_BUILD_MONTH_FIRST_DIGIT, CARRT_BUILD_MONTH_SECOND_DIGIT, CARRT_BUILD_DAY, 0 
+#define CARRTPICO_BUILD_DATE                                                         \
+    CARRT_BUILD_YEAR, CARRT_BUILD_MONTH_FIRST_DIGIT, CARRT_BUILD_MONTH_SECOND_DIGIT, \
+        CARRT_BUILD_DAY, 0
 
-#define CARRTPICO_BUILD_TIME   __TIME__
+#define CARRTPICO_BUILD_TIME __TIME__
 
 constexpr std::uint8_t kCarrtPicoVersionMajor{ CARRTPICO_VERSION_MAJOR };
 constexpr std::uint8_t kCarrtPicoVersionMinor{ CARRTPICO_VERSION_MINOR };
@@ -157,9 +137,12 @@ constexpr char kCarrtPicoFeatures[]{ CARRTPICO_FEATURES };
 constexpr char kCarrtPicoBuildHashFull[]{ CARRTPICO_GIT_HEAD_SHA1_FULL };
 constexpr char kCarrtPicoBuildHashShort[]{ CARRTPICO_GIT_HEAD_SHA1_SHORT };
 
+int getCarrtPicoBuildDateVal();
+int getCarrtPicoBuildHashShortVal();
+
 // Clean up
-#undef CARRTPICO_VERSION_MAJOR 
-#undef CARRTPICO_VERSION_MINOR 
+#undef CARRTPICO_VERSION_MAJOR
+#undef CARRTPICO_VERSION_MINOR
 #undef CARRTPICO_VERSION_REVISION
 #undef CARRTPICO_VERSION
 #undef CARRTPICO_BUILD_DATE
@@ -168,5 +151,4 @@ constexpr char kCarrtPicoBuildHashShort[]{ CARRTPICO_GIT_HEAD_SHA1_SHORT };
 #undef CARRTPICO_GIT_HEAD_SHA1_FULL
 #undef CARRTPICO_GIT_HEAD_SHA1_SHORT
 
-
-#endif  // BuildInfo_h
+#endif    // BuildInfo_h
