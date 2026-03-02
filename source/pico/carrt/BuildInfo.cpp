05a49bd369be435c6f303d4cb3e9d40e23604974
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2026-03-01 22:35:36 -0500

    Add functions to convert numeric strings to values

---
+++ source/pico/carrt/BuildInfo.cpp
@@ -0,0 +1,28 @@
+/*
+    BuildInfo.cpp - Contains implementation of build information related
+    functions for CARRTv3
+
+    Copyright (c) 2026 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "BuildInfo.h"
+
+#include <cstdlib>
+#include <string>
+
+int getCarrtPicoBuildDateVal() { return std::atoi( kCarrtPicoBuildDate ); }
+
+int getCarrtPicoBuildHashShortVal() { return std::stoi( kCarrtPicoBuildDate, nullptr, 16 ); }
